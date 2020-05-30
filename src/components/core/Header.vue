da539ad89bab9e9d75bd17c5a9c85216d83fff9a
Dominic Marcelino <domi.m.98@googlemail.com>
2020-05-30 21:02:30 +0200

    Add routing and nav

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -1,33 +1,79 @@
 <template>
-  <v-app-bar
-    app
-    color="primary"
-    dark
-  >
-    <div class="d-flex align-center">
-      <v-btn
-        icon
-        class="mr-4"
-        :to="{ name: 'Home'}"
+  <div>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        bottom
+        temporary
       >
-        <v-icon>
-          mdi-palette-swatch
-        </v-icon>
-      </v-btn>
+        <v-list
+          nav
+        >
+          <v-list-item
+            v-for="(menuItem, i) in mainMenuItems"
+            :key="i"
+            :to="{ name: menuItem.routeName }"
+            :exact="menuItem.exact || false"
+          >
+            <v-list-item-icon>
+              <v-icon> {{ menuItem.icon }} </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ menuItem.title }}</v-list-item-title>
+          </v-list-item>
 
-      <h2>Project-Name</h2>
-    </div>
+        </v-list>
+      </v-navigation-drawer>
 
-    <v-spacer></v-spacer>
-
-    <v-btn
-      class="ma-2"
-      outlined
-      :to="{ name: 'Versions'}"
+    <v-app-bar
+      app
+      color="primary"
+      dark
     >
-      {{ appVersion }}
-    </v-btn>
-  </v-app-bar>
+      <v-app-bar-nav-icon
+        class="hidden-sm-and-up"
+        @click.stop="drawer = !drawer"
+      />
+
+      <div class="d-flex align-center mr-3">
+        <v-btn
+          icon
+          class="mr-4"
+          :to="{ name: 'Home'}"
+        >
+          <v-icon>
+            mdi-palette-swatch
+          </v-icon>
+        </v-btn>
+
+        <h2>Project-Name</h2>
+      </div>
+
+      <v-toolbar-items class="hidden-xs-only">
+          <v-btn
+            text
+            v-for="(menuItem, i) in mainMenuItems"
+            :key="i"
+            :to="{ name: menuItem.routeName }"
+            :exact="menuItem.exact || false"
+          >
+            <span>{{ menuItem.title }}</span>
+            <v-icon> {{ menuItem.icon }} </v-icon>
+
+          </v-btn>
+        </v-toolbar-items>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="ma-2"
+        outlined
+        :to="{ name: 'Version'}"
+        exact
+      >
+        {{ appVersion }}
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
@@ -45,7 +91,25 @@ export default {
 
   data() {
     return {
-      //
+      drawer: false,
+      mainMenuItems: [
+        {
+          title: 'Home',
+          icon: 'mdi-home',
+          routeName: 'Home',
+          exact: true,
+        },
+        {
+          title: 'About',
+          icon: 'mdi-information',
+          routeName: 'About',
+        },
+        {
+          title: 'Version',
+          icon: 'mdi-information',
+          routeName: 'Version',
+        },
+      ],
     };
   },
 };
--- src/views/Versions.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an Versioning page</h1>
-  </div>
-</template>
